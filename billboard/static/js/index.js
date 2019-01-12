cd675e8c06f4d9954fc10a2245f61d889fcafd76
Michael Schulman <michael.schulman88@gmail.com>
2019-01-12 20:32:20 +0200

    backup

---
+++ billboard/static/js/index.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    $('#login').attr('href', 'login');
+    $('#register').attr('href', 'registration');
+});
--- billboard/urls.py
+++ billboard/urls.py
@@ -7,6 +7,7 @@ from django.contrib.auth.views import login,logout
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, kwargs={'next_page': '/'}, name='logout'),
+    url(r'^logout/$', views.logout, kwargs={'next_page': '/hello'}, name='logout'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^hello/$', views.hello, name='hello'),
 ]
